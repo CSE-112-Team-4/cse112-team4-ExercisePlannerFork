069fb0194d574d26705df7ad72f562cc897e9424
rsa1to <ryutechncode@gmail.com>
2024-05-17 00:09:07 -0700

    #34 new file for local storage

---
+++ source/scripts/localdata.js
@@ -0,0 +1,28 @@
+function loadCardData() {
+  // Retrieve data from local storage
+  const storedData = localStorage.getItem("exerciseCardData");
+
+  // If data exists, parse it from JSON and return
+  if (storedData) {
+    return JSON.parse(storedData);
+  } else {
+    // If no data exists, return an empty array or any default value
+    return [];
+  }
+}
+
+function repopulateCards(savedData) {
+
+  // Loop through the saved data and create/populate cards
+  savedData.forEach((cardData) => {
+    const newExerciseCard = document.createElement("exercise-cards");
+    newExerciseCard.calories = cardData.calories;
+    newExerciseCard.duration = cardData.duration;
+    newExerciseCard.time = cardData.time;
+    newExerciseCard.notes = cardData.notes;
+
+    // Append the populated card to the container
+    const scheduleContainer = document.getElementById("scheduledContainer");
+    scheduleContainer.appendChild(newExerciseCard);
+  });
+}
