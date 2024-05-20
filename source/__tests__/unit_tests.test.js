602c91817b6a990aa919d77c597ddd36a1817213
DanielMilesGH <dmiles@ucsd.edu>
2024-05-20 12:31:22 -0700

    some tests not working, maybe because not initialized correctly?

--- source/__tests__/unit_tests.test.js
+++ source/__tests__/unit_tests.test.js
@@ -1,18 +1,58 @@
-// const functions = require('../scripts/exports.js')
+/**
+ * @jest-environment jsdom
+ */
 
-// ExerciseCard tests
+// exercise card test
+describe("ExerciseCard", () => {
+  let card;
+  let templateData;
 
-const { JSDOM } = require('jsdom');
-const { document } = (new JSDOM('<!DOCTYPE html><html><body></body></html>')).window;
+  beforeEach(async () => {
+    // Create a new exercise card element
+    card = document.createElement("exercise-card");
+    document.body.appendChild(card);
 
-describe('ExerciseCard', () => {
-  let exerciseCard;
-
-  beforeEach(() => {
-    exerciseCard = document.createElement('exercise-card');
-    document.body.appendChild(exerciseCard); 
+    // Wait for the template to load (use a custom event)
+    // await new Promise((resolve) => {
+    //   card.addEventListener("template-loaded", resolve);
+    // });
   });
 
-  // the tests
-});
+  afterEach(() => {
+    document.body.removeChild(card);
+  });
 
+  it("should fetch and render the card template", () => {
+    expect(card.innerHTML).toEqual(templateData);
+  });
+
+  it("should generate a unique ID on creation", () => {
+    const card2 = document.createElement("exercise-card");
+    expect(card.id).not.toEqual(card2.id);
+  });
+
+  it("should correctly get and set properties", () => {
+    card.calories = "250";
+    card.duration = "45";
+    card.time = "10:30 AM";
+    card.notes = "Remember to stretch!";
+
+    expect(card.calories).toEqual("250");
+    expect(card.duration).toEqual("45");
+    expect(card.time).toEqual("10:30 AM");
+    expect(card.notes).toEqual("Remember to stretch!");
+  });
+
+  it("should return the correct card data object", () => {
+    const expectedData = {
+      id: card.id,
+      calories: "", // Initial value is empty
+      duration: "",
+      time: "",
+      notes: "",
+    };
+    const actualData = card.cardData()
+    expect(actualData).toEqual(expectedData);
+  });
+
+});
