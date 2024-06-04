5e91cbb215eb5443facfe8094b539d9379c8d65a
DanielMilesGH <dmiles@ucsd.edu>
2024-06-03 21:20:58 -0700

    got rid of placeholder comments in Cypress

--- cypress/e2e/initial-tests/ui_tests.cy.js
+++ cypress/e2e/initial-tests/ui_tests.cy.js
@@ -1,11 +1,5 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
@@ -23,8 +17,6 @@ describe('basic UI tests', () => {
 
   it('starts with no scheduled', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
     cy.get('#scheduled-container > h3')      
       .should('contain', 'Scheduled')     // Ensure it contains "Scheduled"
       .should('have.length', 1)          // Ensure there's only one <h3>
